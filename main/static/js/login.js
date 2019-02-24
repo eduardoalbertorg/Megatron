8911e0a4669f21c88aa05137a5be01aebab8343f
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2019-02-23 19:12:49 -0600

    Create login and sidebard views

---
+++ main/static/js/login.js
@@ -0,0 +1,35 @@
+new Vue({el: '#app'});
+
+export default {
+    data: () => ({
+        valid: true,
+        username: '',
+        username_rules: [
+            v => !!v || 'Ingresa el codigo de usuario',
+            v => (v && v.length <= 10) || 'Debe de contener menos de 10 caracteres'
+        ],
+        email: '',
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        password: '',
+        password_rules: [
+            v => !!v || 'Ingresa tu contraseña',
+        ],
+    }),
+
+    methods: {
+        validate() {
+            if (this.$refs.form.validate()) {
+                this.snackbar = true
+            }
+        },
+        reset() {
+            this.$refs.form.reset()
+        },
+        resetValidation() {
+            this.$refs.form.resetValidation()
+        }
+    }
+}
---
+++ main/static/js/main.js
@@ -0,0 +1,9 @@
+var main_vue = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            {title: 'Home', icon: 'dashboard'},
+            {title: 'About', icon: 'question_answer'}
+        ],
+    }
+});
